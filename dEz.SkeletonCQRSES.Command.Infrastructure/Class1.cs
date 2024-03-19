f0dff44604e8f6402c2dfd8b819423564182a7c8
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-03-19 23:34:58 +0100

    feat: command dispatcher implementation

---
+++ dEz.SkeletonCQRSES.Command.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace dEz.SkeletonCQRSES.Command.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ dEz.SkeletonCQRSES.Command.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,43 @@
+﻿
+using dEz.SkeletonCQRSES.ES.Core.Commands;
+using dEz.SkeletonCQRSES.ES.Core.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dEz.SkeletonCQRSES.Command.Infrastructure.Dispatchers
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        /// <summary>
+        /// Registered handlers.
+        /// </summary>
+        private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = new();
+
+        /// <inheritdoc cref="ICommandDispatcher"/>
+        public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
+        {
+            if (_handlers.ContainsKey(typeof(T)))
+            {
+                throw new IndexOutOfRangeException("You cannot register the same command handler twice");
+            }
+
+            _handlers.Add(typeof(T), x => handler((T)x));
+        }
+
+        /// <inheritdoc cref="ICommandDispatcher"/>
+        public async Task SendAsync(BaseCommand command)
+        {
+            if (_handlers.TryGetValue(command.GetType(), out Func<BaseCommand, Task> handler))
+            {
+                await handler(command);
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(handler), "No command handler was registered");
+            }
+        }
+    }
+}
