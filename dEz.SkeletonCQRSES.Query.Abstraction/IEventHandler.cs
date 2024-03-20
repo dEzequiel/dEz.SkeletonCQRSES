4257ecc00bb13b873d1a802e66fa0cc502744223
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-03-20 01:45:46 +0100

    feat: event handler implementation

---
+++ dEz.SkeletonCQRSES.Query.Abstraction/IEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using dEz.SkeletonCQRSES.Shared.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dEz.SkeletonCQRSES.Query.Abstraction
+{
+    /// <summary>
+    /// Abstraction throught company related event objects can be handled once they have been consumed from kafka.
+    /// </summary>
+    public interface IEventHandler
+    {
+        Task On(CompanyCreatedEvent @event);
+    }
+}
---
+++ dEz.SkeletonCQRSES.Query.Infrastructure/Handlers/EventHandler.cs
@@ -0,0 +1,27 @@
+﻿using dEz.SkeletonCQRSES.Query.Abstraction;
+using dEz.SkeletonCQRSES.Query.Domain.Services;
+using dEz.SkeletonCQRSES.Shared.DTOs;
+using dEz.SkeletonCQRSES.Shared.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dEz.SkeletonCQRSES.Query.Infrastructure.Handlers
+{
+    public class EventHandler : IEventHandler
+    {
+        private readonly ICompanyService _service;
+        public EventHandler(ICompanyService service)
+        {
+            _service = service;
+        }
+
+        public async Task On(CompanyCreatedEvent @event)
+        {
+            var company = new CompanyForAdd(@event.Id, @event.Name, @event.Address, @event.Country);
+            await _service.AddAsync(company);
+        }
+    }
+}
