b09e18dcae75cdfbbbd13b616c44f6d295a46bb9
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-03-20 01:27:57 +0100

    feat: define dtos

---
+++ dEz.SkeletonCQRSES.Shared/DTOs/CompanyDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dEz.SkeletonCQRSES.Shared.DTOs
+{
+    /// <summary>
+    /// DTO for retrieving Company.
+    /// </summary>
+    /// <param name="Id"></param>
+    /// <param name="Name"></param>
+    /// <param name="FullAddress"></param>
+    public record CompanyForGet(Guid Id, string Name, string FullAddress);
+
+    /// <summary>
+    /// DTO for inserting Company.
+    /// </summary>
+    /// <param name="Name"></param>
+    /// <param name="Address"></param>
+    /// <param name="Country"></param>
+    public record CompanyForAdd(string Name, string Address, string CountrY);
+
+    /// <summary>
+    /// DTO for updating Company.
+    /// </summary>
+    /// <param name="Name"></param>
+    /// <param name="Address"></param>
+    /// <param name="Country"></param>
+    public record CompanyForUpdate(string Name, string Address, string Country);
+}
