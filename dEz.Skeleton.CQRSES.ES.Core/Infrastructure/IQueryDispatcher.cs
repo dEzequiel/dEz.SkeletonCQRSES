6709fbd493cbbc577fd0787c0925a0d863745839
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-03-21 00:35:09 +0100

    feat: query dispatcher definition

---
+++ dEz.Skeleton.CQRSES.ES.Core/Infrastructure/IQueryDispatcher.cs
@@ -0,0 +1,26 @@
+﻿using dEz.SkeletonCQRSES.ES.Core.Queries;
+
+namespace dEz.SkeletonCQRSES.ES.Core.Infrastructure
+{
+    /// <summary>
+    /// Interface for dispatching queries.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity the queries operate on.</typeparam>
+    public interface IQueryDispatcher<TEntity>
+    {
+        /// <summary>
+        /// Registers a handler for a specific query.
+        /// </summary>
+        /// <typeparam name="TQuery">The type of query to handle.</typeparam>
+        /// <param name="handler">The handler function for the query.</param>
+        /// <returns></returns>
+        void RegisterHandler<TQuery>(Func<TQuery, Task<IEnumerable<TEntity>>> handler) where TQuery : BaseQuery;
+
+        /// <summary>
+        /// Asynchronously dispatch a query.
+        /// </summary>
+        /// <param name="query">The query to send.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task<IEnumerable<TEntity>> SendAsync(BaseQuery query);
+    }
+}
