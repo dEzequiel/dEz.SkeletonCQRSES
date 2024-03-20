82f291b0838db2f00e35dd77633892e6636e4340
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-03-20 15:32:23 +0100

    Revert "fix"
    This reverts commit 26cdb966c073ed88781cf69fa9bfff801209dbe4.

---
+++ Core/Infrastructure/IEventStore.cs
@@ -0,0 +1,30 @@
+﻿using dEz.SkeletonCQRSES.ES.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dEz.SkeletonCQRSES.ES.Core.Infrastructure
+{
+    public interface IEventStore
+    {
+        /// <summary>
+        /// Persist a new event asynchronously.
+        /// 
+        /// Saves all unsafe changes to the aggregate in the form of events.
+        /// </summary>
+        /// <param name="aggregateId"></param>
+        /// <param name="events"></param>
+        /// <param name="expectedVersion"></param>
+        /// <returns></returns>
+        Task SaveEventAsync(Guid aggregateId, string aggregateType, IEnumerable<BaseEvent> events, int expectedVersion);
+
+        /// <summary>
+        /// Return all events realted to an aggregate asynchronously.
+        /// </summary>
+        /// <param name="aggregateId"></param>
+        /// <returns></returns>
+        Task<IEnumerable<BaseEvent>> GetEventsAsync(Guid aggregateId);
+    }
+}
