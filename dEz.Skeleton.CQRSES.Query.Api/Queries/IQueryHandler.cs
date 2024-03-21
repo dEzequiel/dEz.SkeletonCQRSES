d12001451f09acfffbe9fa094d3dfedb81e69d53
Ezequiel Dlr <linezekdlr@hotmail.com>
2024-03-21 01:12:38 +0100

    feat: query handler implementation

---
+++ dEz.Skeleton.CQRSES.Query.Api/Queries/IQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using dEz.SkeletonCQRSES.Query.Domain.Entities;
+
+namespace dEz.Skeleton.CQRSES.Query.Api.Queries
+{
+    /// <summary>
+    /// Interface for handling queries.
+    /// </summary>
+    public interface IQueryHandler
+    {
+        /// <summary>
+        /// Handler for retrieving company by id.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        Task<Company> HandleAsync(FindCompanyById query);
+        
+        /// <summary>
+        /// Handler for retrieving all companies.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        Task<IEnumerable<Company>> HandleAsync(FindAllCompaniesQuery query);
+    }
+}
---
+++ dEz.Skeleton.CQRSES.Query.Api/Queries/QueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using dEz.SkeletonCQRSES.Query.Domain.Entities;
+using dEz.SkeletonCQRSES.Query.Domain.Services;
+
+namespace dEz.Skeleton.CQRSES.Query.Api.Queries
+{
+    /// <summary>
+    /// Implementation for handling queries.
+    /// </summary>
+    public class QueryHandler : IQueryHandler
+    {
+        private readonly ICompanyService _companyService;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="companyService"></param>
+        public QueryHandler(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+
+        /// <inheritdoc cref="IQueryHandler"/>
+        public async Task<Company> HandleAsync(FindCompanyById query)
+        {
+            var company = await _companyService.GetByIdAsync(query.Id);
+            return company;
+        }
+
+        /// <inheritdoc cref="IQueryHandler"/>
+        public async Task<IEnumerable<Company>> HandleAsync(FindAllCompaniesQuery query)
+        {
+            var companies = await _companyService.GetAllAsync();
+            return companies;
+        }
+    }
+}
